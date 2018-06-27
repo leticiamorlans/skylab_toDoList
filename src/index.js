cd16ba723d5a55bcd222ecaa0f8a3c21bbd168fa
Leticia Morlans <letgomorlans@gmail.com>
2018-06-27 23:16:36 +0200

    added js for todolist

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import './index.scss';
+
+//let todoList = [];
+//crear objeto lista
+//todoList = new TodoList();
+
+$(document).ready(function(){
+
+    //Create <li> when adding item in the form 
+    var appendListItem = () => {
+        $('.add').click((e) => {
+            e.preventDefault;
+
+            let listItem = $('#to-do').val();
+            
+            if(listItem != '') {
+                $('#to-do-list').append(
+                    `<li class="to-do-list__item">
+                        ${listItem}
+                        <span class="deleteItem">×</span>
+                    </li>`
+                );
+            }
+            $('#to-do').focus().val('');
+        });
+        
+    };
+    
+    appendListItem();
+
+    // Add class done al <li> when clicking on
+    $('.to-do-list__item').click(function(){
+        $(this).toggleClass("done");  
+    });
+    
+
+    // Delete <li> when clicking in deleteItem
+    $('.deleteItem').click(function(e){
+        e.stopPropagation();
+        $(this).parent().remove();
+    });
+
+});
+
+// var spy = spyOn(toDoList, 'newElement' )
